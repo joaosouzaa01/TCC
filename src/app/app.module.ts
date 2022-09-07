f99230511d92c20fdeb836aea2759db5d35c1b6c
Nebularsty <nicolasoliveira9635@gmail.com>
2022-09-07 16:15:20 -0300

    criação do component card, BORA BILL!

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,13 +6,15 @@ import { AppComponent } from './app.component';
 import { HomeComponent } from './pages/home/home.component';
 import { MonteOSeuComponent } from './pages/monte-o-seu/monte-o-seu.component';
 import { SlideshowComponent } from './shared/components/slideshow/slideshow.component';
+import { CardsComponent } from './shared/components/cards/cards.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HomeComponent,
     MonteOSeuComponent,
-    SlideshowComponent
+    SlideshowComponent,
+    CardsComponent
   ],
   imports: [
     BrowserModule,
