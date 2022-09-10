f397c600351ce268e46cc44ee19ac34014c8a237
jajãoo <88406996+joaosouzaa01@users.noreply.github.com>
2022-09-10 13:14:50 -0300

    Banner funcionando

--- src/app/shared/components/slideshow/slideshow.component.ts
+++ src/app/shared/components/slideshow/slideshow.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { max } from 'rxjs';
 
 @Component({
   selector: 'app-slideshow',
@@ -7,27 +8,68 @@ import { Component, OnInit } from '@angular/core';
 })
 export class SlideshowComponent implements OnInit {
 
+ private max: Number = 0;
+
   constructor() { }
 
   ngOnInit(): void {
-    this.showSlides()
+
+    // this.showSlides()
+
+      let time = 5000,
+    currentImageIndex = 0,
+    images = document
+                .querySelectorAll("#slider img")
+    this.max = images.length;
+    const valorMax = this.max;
+
+function nextImage() {
+
+    images[currentImageIndex]
+        .classList.remove("selected")
+
+    currentImageIndex++
+
+    if(currentImageIndex >= valorMax )
+        currentImageIndex = 0
+
+    images[currentImageIndex]
+        .classList.add("selected")
+}
+
+function start() {
+    setInterval(() => {
+        // troca de image
+        nextImage()
+    }, time)
+}
+
+window.addEventListener("load", start)
   }
-  
-  showSlides() {
-    let slideIndex = 0;
-    let i;
-    let slides = document.getElementsByClassName("mySlides") as HTMLCollectionOf<HTMLElement>;
-    let dots = document.getElementsByClassName("dot");
-    for (i = 0; i < slides.length; i++) {
-    slides[i].style.display = "none";  
-    }
-    slideIndex++;
-    if (slideIndex > slides.length) {slideIndex = 1}    
-    for (i = 0; i < dots.length; i++) {
-    dots[i].className = dots[i].className.replace(" active", "");
-    }
-    slides[slideIndex-1].style.display = "block";  
-    dots[slideIndex - 1].className += " active";
-   setTimeout(this.showSlides, 2000);
-  }
+
+
+
+
+
+
+
+
+
+//   showSlides() {
+//     let slideIndex = 0;
+//     let i;
+//     let slides = document.getElementsByClassName("mySlides") as HTMLCollectionOf<HTMLElement>;
+//     let dots = document.getElementsByClassName("dot");
+//     for (i = 0; i < slides.length; i++) {
+//     slides[i].style.display = "none";  
+//     }
+//     slideIndex++;
+//     if (slideIndex > slides.length) {slideIndex = 1}    
+//     for (i = 0; i < dots.length; i++) {
+//     dots[i].className = dots[i].className.replace(" active", "");
+//     }
+//     slides[slideIndex-1].style.display = "block";  
+//     dots[slideIndex - 1].className += " active";
+//    setTimeout(this.showSlides, 2000);
+//   }
 }
